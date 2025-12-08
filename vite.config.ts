1e0fe7b98649b5983c821ad57cf98e68e302b11a
Sharad Bhat <sharad.mbhat@gmail.com>
2025-12-08 15:07:08 -0800

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,31 +8,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      manifest: {
-        name: "PlantLog",
-        short_name: "PlantLog",
-        description: "Plant care logger",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "/plant_64_blank.png",
-            sizes: "64x64",
-            type: "image/png",
-          },
-          {
-            src: "/plant_512_blank.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/plant_512_blank.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
