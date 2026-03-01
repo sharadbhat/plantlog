1f08e0fa3c4c336b5e8b2d8fc6ac74b77365adc3
Sharad Bhat <sharad.mbhat@gmail.com>
2026-02-28 22:28:03 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
           </AppShell.Header>
           <AppShell.Main>
             <CardList />
-            {navigator.brave?.isBrave?.()}
+            {navigator?.brave?.isBrave?.()}
           </AppShell.Main>
           <AppShell.Footer
             withBorder={false}
